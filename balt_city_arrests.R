89bb4cd924fda94ea608e27592fadaa93c4cf744
Cory Cone <ccone@mica.edu>
2021-03-10 17:19:03 -0500

    create 2020 through today dataset

--- balt_city_arrests.R
+++ balt_city_arrests.R
@@ -4,3 +4,14 @@ library(lubridate)
 arrests.df <- read.csv("Arrests.csv", stringsAsFactors = FALSE)
 
 str(arrests.df)
+
+head(arrests.df)
+
+n_distinct(arrests.df$ArrestNumber)
+
+#fix date data type
+arrests.df$ArrestDateTime <- ymd_hms(arrests.df$ArrestDateTime)
+
+arrests_2020_2021.df <- arrests.df %>% filter(ArrestDateTime > '2019-12-26 20:47:00')
+
+
