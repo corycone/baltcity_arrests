82403133065c31b247306611ad9443c155e3dd31
Cory Cone <ccone@mica.edu>
2021-03-10 22:55:36 -0500

    in progress simple bar. Split out years next

--- balt_city_arrests.R
+++ balt_city_arrests.R
@@ -12,6 +12,15 @@ n_distinct(arrests.df$ArrestNumber)
 #fix date data type
 arrests.df$ArrestDateTime <- ymd_hms(arrests.df$ArrestDateTime)
 
-arrests_2020_2021.df <- arrests.df %>% filter(ArrestDateTime > '2019-12-26 20:47:00')
+arrests_2020_2021.df <- arrests.df %>% filter(ArrestDateTime > '2019-12-26 20:47:00' & ArrestDateTime > '2021-01-01 20:47:00' )
+
+
+arrests_by_week.df <- arrests_2020_2021.df %>% group_by(week(ArrestDateTime)) %>% summarise(n = n_distinct(ArrestNumber))
+
+colnames(arrests_by_week.df) <- c("week", "arrests")
+
+#in progress
+ggplot(arrests_by_week.df, aes(x = week, y = arrests)) +
+  geom_bar(stat = "identity")
 
 
